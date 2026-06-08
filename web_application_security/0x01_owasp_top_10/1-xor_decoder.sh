0292c3f670d8325838f93eb0b0190b85a3317770
Raja Pandi Perumal <rajaskyrider@gmail.com>
2026-06-08 14:25:30 +0200

    ex0

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -7,9 +7,31 @@ while [ "$i" -lt "${#mask}" ]
 do
 	char="${mask:i:1}"
 	ascii=$(printf "%d" "'$char")
-	val=$((ascii + key - 2 * (ascii & key)))
-	octal=$(printf '%03o' "$val")
-	printf "\\$octal"
+	a="$ascii"
+	b="$key"
+	val=0
+	factor=1
+	bit=0
+
+	while [ "$bit" -lt 8 ]
+	do
+		abit=$((a % 2))
+		bbit=$((b % 2))
+		xorbit=$((abit + bbit))
+
+		if [ "$xorbit" -eq 1 ]
+		then
+			val=$((val + factor))
+		fi
+
+		a=$((a / 2))
+		b=$((b / 2))
+		factor=$((factor * 2))
+		bit=$((bit + 1))
+	done
+
+	octal=$(printf "%03o" "$val")
+	printf "%b" "\\$octal"
 	i=$((i + 1))
 done
 echo
